3faac17cda1406d5de9791f21dbc635aac3f627c
Nicolas Riscalas <nicolas.riscalas@stu.ocsb.ca>
2023-05-10 03:01:03 +0000

    created the bubble sort program and added comments

---
+++ com/example/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * This package contains classes for performing various string operations.
+ * <p>
+ * The classes in this package provide functionality for tasks such as string
+ * manipulation, searching, and parsing.
+ * </p>
+ * <p>
+ * To use the classes in this package, simply import the package using the
+ * import statement:
+ * </p>
+ * <pre>
+ * import com.example;
+ * </pre>
+ * <p>
+ * For more information on the classes and their usage, see the documentation
+ * for each class.
+ * </p>
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
+package com.example;
